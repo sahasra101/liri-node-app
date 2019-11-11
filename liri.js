8c831ee180310f69c961c99996e6a045c6623e4b
sahasra101 <ajaykiri7@gmail.com>
2019-11-10 19:00:46 -0500

    started coding JS file

--- liri.js
+++ liri.js
@@ -1,3 +1,48 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+var inquirer = require("inquirer");
+var axios = require("axios");
+var moment = require("moment");
+
+
+var userCommand = process.argv[2];
+// confirm that index 2 is the command
+console.log(userCommand);
+var userQuery = process.argv[3]; // splice after index 2
+var userQuery = [];
+for (i = 3; i < process.argv.length; i++) {
+    userQuery.push(process.argv[i]);
+}
+userQuery = uQuery.join(" ");
+// confirm that index 3 and after are userQuery
+console.log(userQuery);
+
+function runCommand() {
+
+    // if (userCommand == "concert-this") {
+    //     function askBandsInTown();
+    // }
+
+    if (userCommand == "spotify-this-song") {
+        spotify.search({ type: 'track', query: 'All the Small Things'}, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+
+            console.log(data);
+        });
+    // }
+
+    // if (userCommand == "movie-this") {
+    //     function omdb();
+    // }
+
+    // if (userCommand == "do-what-it-says") {
+
+    // }
+}
+runCommand();
+
+// otherwise display please try again with
