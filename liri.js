2fd66d88187beee32b5d51aff526852bdf8b9850
sahasra101 <ajaykiri7@gmail.com>
2019-11-10 21:06:14 -0500

    added switch function and functions for each command

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 var inquirer = require("inquirer");
 var axios = require("axios");
 var moment = require("moment");
@@ -10,39 +10,96 @@ var moment = require("moment");
 var userCommand = process.argv[2];
 // confirm that index 2 is the command
 console.log(userCommand);
-var userQuery = process.argv[3]; // splice after index 2
-var userQuery = [];
+var uQuery = [];
 for (i = 3; i < process.argv.length; i++) {
-    userQuery.push(process.argv[i]);
+    uQuery.push(process.argv[i]);
 }
 userQuery = uQuery.join(" ");
 // confirm that index 3 and after are userQuery
 console.log(userQuery);
 
-function runCommand() {
+switch (userCommand) {
+    case "concert-this":
+        checkBandsApi(userQuery);
+        break;
 
-    // if (userCommand == "concert-this") {
-    //     function askBandsInTown();
-    // }
+    case "spotify-this-song":
+        checkSpotify(userQuery);
+        break;
 
-    if (userCommand == "spotify-this-song") {
-        spotify.search({ type: 'track', query: 'All the Small Things'}, function (err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
+    case "movie-this":
+        checkOmbd(userQuery);
+        break;
 
-            console.log(data);
-        });
-    // }
-
-    // if (userCommand == "movie-this") {
-    //     function omdb();
-    // }
-
-    // if (userCommand == "do-what-it-says") {
-
-    // }
+    // case "do-what-it-says":
+    //     outputNum = parseFloat(num1) / parseFloat(num2);
+    //     break;
+    default:
+        console.log("Please select a valid command:\nconcert-this\nspotify-this-song\nmovie-this\ndo-what-it-says");
 }
-runCommand();
 
-// otherwise display please try again with
+function checkBandsApi() {
+    console.log(`\nThe artist you picked was ${userQuery}\n`);
+    axios.get("https://rest.bandsintown.com/artists/" + userQuery.toLowerCase().replace(" ", "+") + "/events?app_id=codingbootcamp")
+        .then(
+            function (response) {
+                if (response.data.length === 0) {
+                    console.log(`${userQuery} has no concerts found in the near future.`);
+                } else {
+                    for (let i = 0; i < response.data.length; i++) {
+                        console.log(`Venue Name: ${response.data[i].venue.name}`);
+                        console.log(`Venue Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}`);
+                        console.log(`Date: ${moment(response.data[i].datetime).format("MM/DD/YYYY")}`);
+                        console.log("_____________________________________");
+                    }
+                }
+            })
+        .catch(function (error) {
+            console.log("Error! Please try again!");
+        });
+}
+
+function checkSpotify() {
+    console.log(`\nThe song you picked was ${userQuery}\n`);
+    spotify
+        .search({ type: 'track', query: userQuery.toLowerCase().replace(" ", "+"), limit:5 })
+        .then(function (response) {
+            if (response.tracks.items.length === 0) {
+                console.log(`No results found for your search of ${userQuery}`);
+            } else {
+                for (let j = 0; j < response.tracks.items.length; j++) {
+                    console.log(`Artist: ${response.tracks.items[j].album.artists[0].name}`);
+                    console.log(`Song's name: ${response.tracks.items[j].name}`);
+                    console.log(`Album name: ${response.tracks.items[j].album.name}`);
+                    console.log(`Preview Link: ${response.tracks.items[j].album.artists[0].external_urls.spotify}`);
+                    console.log("__________________________________________________________");
+                }
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+function checkOmbd() {
+    console.log(`The movie you picked was ${userQuery}\n`);
+    axios.get("http://www.omdbapi.com/?t=" + userQuery.toLowerCase().replace(" ", "+") + "&y=&plot=short&apikey=trilogy")
+        .then(
+            function (response) {
+                if (!response.data.Title) {
+                    console.log(`No results found for your search of ${userQuery}`);
+                } else {
+                    console.log(`\nTitle: ${response.data.Title}`);
+                    console.log(`Year: ${response.data.Year}`);
+                    console.log(`IMDB Rating: ${response.data.imdbRating}`);
+                    console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+                    console.log(`Country of production: ${response.data.Country}`);
+                    console.log(`Language: ${response.data.Language}`);
+                    console.log(`Plot: ${response.data.Plot}`);
+                    console.log(`Actors: ${response.data.Actors}`);
+                }
+            })
+        .catch(function (error) {
+            console.log("Error! Please try again!");
+        });
+}
